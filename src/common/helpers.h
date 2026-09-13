de840142287841feb634e04dcb24ad0960240e85
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-09-13 21:50:27 +0900

    avoid double divided by float

--- src/common/helpers.h
+++ src/common/helpers.h
@@ -6,4 +6,4 @@
 // Get the high nibble of a byte.
 #define HIGH_NIBBLE(b) (((b) >> 4) & 0x0F)
 
-#define BYTES_TO_KILOBYTES(bytes) ((double)(bytes) / 1000.0f)
+#define BYTES_TO_KILOBYTES(bytes) ((double)(bytes) / 1000.0)
