d6098411accf6f846ce2f6439e39e0dda9980c66
Anthony Sharp <anthonysharp15@gmail.com>
2026-09-26 19:12:39 +0900

    remove unused HIGH_NIBBLE macro

--- src/common/helpers.h
+++ src/common/helpers.h
@@ -3,7 +3,4 @@
 // Get the low nibble of a byte.
 #define LOW_NIBBLE(b) ((b) & 0x0F)
 
-// Get the high nibble of a byte.
-#define HIGH_NIBBLE(b) (((b) >> 4) & 0x0F)
-
 #define BYTES_TO_KILOBYTES(bytes) ((double)(bytes) / 1000.0)
